31919329157ac65153c24021bd1aa7ab34a2e60a
Jacques Serizay <jacquesserizay@gmail.com>
2020-10-24 00:11:56 +0200

    cleaned up matrix computing

--- metator/bin/matrix.sh
+++ metator/bin/matrix.sh
@@ -24,21 +24,25 @@ then
     mkdir "${partition_dir}"/network_matrices/
 fi
 
-# Save contigs infos
+# Compute the matrix of pairwise Hamming distances between all pairs of core communities and CC infos
+echo "Computing matrix of Hamming distances for ${iter} interations..."
+#
+communities="${tmp_dir}"/"${project}"_iterations_"${iter}".txt
+paste $(printf "${partition_dir}"/iteration/%s.community $(seq 1 "${iter}")) > "${communities}"
+#
+indices="${tmp_dir}"/"${project}"_indices_"${iter}".txt
+input="${partition_dir}"/partition/core_size_indices_"${iter}".txt
+sed 's,^[ ]*,,' "${input}" | sed 's, ,\t,' | cut -f3 > "${indices}"
+#
 contigs="${partition_dir}"/network_matrices/"${project}"_"${iter}"-iters_contigID-coreID-contigSize.txt
 contigs_info="${partition_dir}"/partition/chunkid_core_size_"${iter}".txt
 paste \
   <(cat ${contigs_info} | cut -f1,2) \
   <(cat "${assembly_dir}"/"${project}"_"${size_contig_threshold}".fa | awk '$0 ~ ">" {if (NR > 1) {print c;} c=0;printf substr($0,2,100) "\t"; } $0 !~ ">" {c+=length($0);} END { print c; }' | cut -f2) \
   > "${contigs}"
-
-# Compute the matrix of pairwise Hamming distances between all pairs of core communities and CC infos
-echo "Computing matrix of Hamming distances for ${iter} interations..."
-input="${partition_dir}"/partition/core_size_indices_"${iter}".txt
-communities="${tmp_dir}"/"${project}"_iterations_"${iter}".txt
-indices="${tmp_dir}"/"${project}"_indices_"${iter}".txt
-sed 's,^[ ]*,,' "${input}" | sed 's, ,\t,' | cut -f3 > "${indices}"
+#
 output="${partition_dir}"/network_matrices/"${project}"_"${iter}"-iters_hamming_distance.npz
+#
 python3 "${scripts_dir}"/hamming.py \
   --communities "${communities}" \
   --indices "${indices}" \
